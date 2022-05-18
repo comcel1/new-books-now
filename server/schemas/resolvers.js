f8b3f0cae43bd81d363ad87fc8a067014f4dcf36
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-05-18 13:40:45 -0500

    added resolvers for users, me and addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,8 +7,21 @@ const resolvers = {
     users: async (parent, args) => {
       return User.find();
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id });
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
   },
-  Mutation: {},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
